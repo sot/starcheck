6c417bf942420491cffcb4ccd87e0d955ee94b73
Jean Connelly <jconnelly@cfa.harvard.edu>
2014-06-02 18:00:09 -0400

    Set Python code to have same version as Perl

--- starcheck/version.py
+++ starcheck/version.py
@@ -14,7 +14,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '0.1'
+version = '11.2dev'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
