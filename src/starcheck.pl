be888efa9e86010923d21cbdd82381a96f687975
Jean Connelly <jconnelly@cfa.harvard.edu>
2016-01-15 19:06:06 -0500

    Set version to 11.6

--- src/starcheck.pl
+++ src/starcheck.pl
@@ -8,7 +8,7 @@
 ##*******************************************************************************
 
 
-my $version = '11.6dev';
+my $version = '11.6';
 
 # Set defaults and get command line options
 
