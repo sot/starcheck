47ae454f67b5071b43fe72cff49f8bca05e52d56
Jean Connelly <jconnelly@cfa.harvard.edu>
2016-04-10 15:10:50 -0400

    Explicitly set MS filter disabled

--- src/starcheck.pl
+++ src/starcheck.pl
@@ -32,6 +32,7 @@ use PoorTextFormat;
 #use GrabEnv qw( grabenv );
 #use Shell::GetEnv;
 
+use Ska::Starcheck::FigureOfMerit qw( set_acq_model_ms_filter );
 use Ska::Starcheck::Obsid;
 use Ska::Parse_CM_File;
 use Carp;
@@ -61,7 +62,6 @@ def plot_cat_wrapper(kwargs):
         # write errors to starcheck's global warnings and STDERR
         perl.warning("Error with Inline::Python imports {}\n".format(err))
     return make_plots_for_obsid(**kwargs)
-
 };
 
 # cheat to get the OS (major)
@@ -331,8 +331,9 @@ Ska::Starcheck::Obsid::set_odb(%odb);
 
 Ska::Starcheck::Obsid::set_config($config_ref);
 
-
-
+# Disable the Multiple Star filter
+my $MSF_ENABLED = 0;
+set_acq_model_ms_filter($MSF_ENABLED);
 
 # Read Maneuver error file containing more accurate maneuver errors
 my @manerr;
@@ -424,6 +425,7 @@ my %guidesumm = Ska::Parse_CM_File::guide($guide_summ) if (defined $guide_summ);
 
 # After all commands have been added to each obsid, set some global
 # object parameters based on commands
+
 
 foreach my $obsid (@obsid_id) {
     $obs{$obsid}->set_obsid(\%guidesumm); # Commanded obsid
@@ -666,7 +668,14 @@ if (%input_files) {
 	$save_hash{run}{badpix} = $ACA_badpix_date;
     }
 
+    if ($MSF_ENABLED){
+        $out .= "Using acquisition model for multiple star filter enabled\n";
+    }
+    else{
+        $out .= "Using acquisition model for multiple star filter disabled\n";
+    }
     $out .= "\n";
+
 }
 
 if (@global_warn) {
