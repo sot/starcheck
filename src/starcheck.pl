2ac5aeb445565244d6ee865374d9cff676c03772
Jean Connelly <jconnelly@cfa.harvard.edu>
2016-08-29 08:16:43 -0400

    Update starcheck version to 11.13

--- src/starcheck.pl
+++ src/starcheck.pl
@@ -8,7 +8,7 @@
 ##*******************************************************************************
 
 
-my $version = '11.12';
+my $version = '11.13';
 
 # Set defaults and get command line options
 
