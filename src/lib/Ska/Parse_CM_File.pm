affef050313a69901bc04aaf621e5254f0f151aa
Jean Connelly <jconnelly@cfa.harvard.edu>
2016-09-07 19:30:21 -0400

    Update ManErr parsing for SEP0516X products

--- src/lib/Ska/Parse_CM_File.pm
+++ src/lib/Ska/Parse_CM_File.pm
@@ -363,7 +363,10 @@ sub man_err {
     open (my $MANERR, $man_err)
 	or die "Couldn't open maneuver error file $man_err for reading\n";
     while (<$MANERR>) {
+        chomp;
 	last if (/total number/i);
+        next unless (/\S/);
+        next if (/^\!Schedule generated/);
 	if ($in_man) {
 	    my @vals = split;
 	    if ($#vals != $#cols) {
