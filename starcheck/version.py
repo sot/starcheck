229b1f82f4a0a21124d0d66cff1fc82a6ae4fd81
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-06-13 15:32:18 -0400

    Update version to 11.26

--- starcheck/version.py
+++ starcheck/version.py
@@ -15,7 +15,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '11.25'
+version = '11.26'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
