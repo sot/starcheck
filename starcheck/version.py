1862cbbbb37c26316ef20d1b1367b09576464d5f
Jean Connelly <jconnelly@cfa.harvard.edu>
2019-06-12 06:40:54 -0400

    Update version to 13.2

--- starcheck/version.py
+++ starcheck/version.py
@@ -15,7 +15,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '13.1'
+version = '13.2'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
