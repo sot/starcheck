5d5cf8f12f7d0c3567478cafbc8646d366261fe3
Jean Connelly <jconnelly@cfa.harvard.edu>
2022-06-09 10:51:25 -0400

    Remove check for bright perigee stars

--- starcheck/src/starcheck.pl
+++ starcheck/src/starcheck.pl
@@ -517,8 +517,7 @@ foreach my $obsid (@obsid_id) {
     $obs{$obsid}->set_proseco_probs_and_check_P2();
     $obs{$obsid}->check_star_catalog($or{$obsid}, $par{vehicle});
     $obs{$obsid}->check_sim_position(@sim_trans) unless $par{vehicle};
-	$obs{$obsid}->check_momentum_unload(\@bs);
-    $obs{$obsid}->check_bright_perigee($radmon);
+    $obs{$obsid}->check_momentum_unload(\@bs);
     $obs{$obsid}->check_guide_count();
 
 # Make sure there is only one star catalog per obsid
