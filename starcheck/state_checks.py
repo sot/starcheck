f964ec602d78e5464aef75ab0263532f7dce790e
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-12-10 21:00:56 -0500

    Simplify NMM calc by assuming NPNT start
    Also adds processing warning if weird continuity initial pcad_mode state.

--- starcheck/state_checks.py
+++ starcheck/state_checks.py
@@ -16,8 +16,16 @@ from Quaternion import Quat
 @lru_cache
 def make_man_table():
     """
-    Calculate the time for range of manuever angles using Chandra.Maneuver
-    duration.
+    Compute a lookup table of maneuver angles and durations.
+
+    This function calculates the durations for a range of maneuver angles and makes an astropy
+    table of the results. The durations are calculated using chandra_maneuver.duration.
+
+    Returns:
+    -------
+    Table
+        An astropy Table containing the durations and corresponding maneuver angles.
+
     """
     durations = []
     q0 = Quat(equatorial=(0, 0, 0))
@@ -33,94 +41,35 @@ def make_man_table():
     return Table([durations, angles], names=["duration", "angle"])
 
 
-def calc_pcad_states_nman_durations(states):
-    """
-    For each NPNT state in a monotonically increasing list of states,
-    calculate the duration of time in NMAN before it
-    and interpolate into the man_table to calculate the equivalent
-    angle for this duration.
-    """
-    man_table = make_man_table()
-
-    nman_durations = []
-    single_nman_duration = 0
-    for state in states:
-        if state["pcad_mode"] == "NMAN":
-            single_nman_duration += state["tstop"] - state["tstart"]
-        if (state["pcad_mode"] == "NPNT") & (single_nman_duration > 0):
-            nman_durations.append(
-                {
-                    "tstart": state["tstart"],
-                    "nman_sum": single_nman_duration.copy(),
-                    "angle": np.interp(
-                        single_nman_duration, man_table["duration"], man_table["angle"]
-                    ),
-                }
-            )
-            # Reset the sum of NMM time to 0 on any NPNT state
-            single_nman_duration = 0
-
-        # If in a funny state, just add a large "maneuver time" of 20ks
-        if state["pcad_mode"] not in ["NMAN", "NPNT"]:
-            single_nman_duration += 20000
-
-    return Table(nman_durations)
-
-
 @lru_cache
-def make_man_angles(backstop_file):
+def get_pcad_states(backstop_file):
     """
-    Calculate the sum of time in NMAN before each NPNT dwell and make a table
-    of the NPNT start times, the sum of time in NMAN before each, and the
-    maneuver-equivalent-angle for each NMAN sum time.
+    Get the pcad_mode kadi command states for the products in review (as defined by the backstop_file
+    and continuity).
 
-    :param: backstop file
-    :returns: astropy Table with columns 'tstart', 'nman_sum', 'angle'
+    Parameters:
+    - backstop_file (str): The path to the backstop file.
+
+    Returns:
+    - states (astropy Table): An Table of states for the available commands.
+
+    Notes:
+    - This function just exists to make this easy to cache.
     """
-    states = get_states(backstop_file, state_keys=["pcad_mode"])
-
-    # If the states begin with NPNT we're done.  Otherwise, get some more states.
-    # 10 days is more than enough: it doesn't matter.
-    if states["pcad_mode"][0] != "NPNT":
-        pre_states = kadi_states.get_states(
-            start=CxoTime(states[0]["tstart"]) - 10 * u.day,
-            stop=states[0]["tstart"],
-            state_keys=["pcad_mode"],
-            merge_identical=True,
-        )
-        states = vstack([pre_states, states])
-
-    out = calc_pcad_states_nman_durations(states)
-    return out
-
-
-def get_obs_man_angle(npnt_tstart, backstop_file, default_large_angle=180):
-    """
-    For the dwell that starts at npnt_tstart, get the maneuver-equivalent-angle
-    the corresponds to the NMAN time before the dwell start.
-
-    :param npnt_start: start time of npnt dwell
-    :param backstop_file: backstop file
-    :param default_large_angle: for weird cases, define the
-         maneuver-equivalent-angle to be this value (default 180 deg)
-    :returns: float value of a maneuver angle between 0 and 180
-    """
-
-    man_angles = make_man_angles(backstop_file)
-
-    idx = np.argmin(np.abs(man_angles["tstart"] - npnt_tstart))
-    dt = man_angles["tstart"][idx] - npnt_tstart
-
-    # For starcheck, if something went wrong, just
-    # use a large (180) angle
-    if np.abs(dt) > 100:
-        return float(default_large_angle)
-
-    # Otherwise index into the table to get the angle for this duration
-    return float(man_angles["angle"][idx])
+    return get_states(backstop_file, state_keys=["pcad_mode"])
 
 
 def get_states(backstop_file, state_keys=None):
+    """
+    Get the states for available commands within a specified time range.
+
+    Parameters:
+    - backstop_file (str): The path to the backstop file.
+    - state_keys (list, optional): A list of state keys to filter the states. Defaults to None.
+
+    Returns:
+    - states (astropy.Table): A Table states.
+    """
     bs_cmds = kadi_commands.get_cmds_from_backstop(backstop_file)
     rltt = bs_cmds.get_rltt() or bs_cmds["date"][0]
 
@@ -129,24 +78,98 @@ def get_states(backstop_file, state_keys=None):
     sched_stop = bs_cmds.get_scheduled_stop_time() or bs_cmds["date"][-1]
 
     # Get the states for available commands.  This automatically gets continuity.
-    return kadi_states.get_states(
+    out = kadi_states.get_states(
         cmds=bs_cmds,
         start=rltt,
         stop=sched_stop,
         state_keys=state_keys,
         merge_identical=True,
     )
+    return out
+
+
+@lru_cache
+def calc_man_angle_for_duration(duration):
+    """
+    Calculate the maneuver-equivalent-angle for a given duration.
+
+    Parameters
+    ----------
+    duration : float
+        The duration for which the maneuver-equivalent-angle needs to be calculated.
+
+    Returns
+    -------
+    float
+        The maneuver-equivalent-angle corresponding to the given duration.
+    """
+    man_table = make_man_table()
+    out = np.interp(duration, man_table['duration'], man_table['angle'])
+    return out
+
+
+def check_first_state_npnt(backstop_file):
+    """
+    Check that the first state is NPNT.
+
+    Parameters
+    ----------
+    backstop_file : str
+        The path to the backstop file.
+
+    Returns
+    -------
+    bool
+        True if the first state is NPNT, False otherwise.
+    """
+    states = get_pcad_states(backstop_file)
+    if states['pcad_mode'][0] != 'NPNT':
+        return False
+    return True
+
+
+def get_obs_man_angle(npnt_tstart, backstop_file):
+    """
+    For the dwell that starts at npnt_tstart, get the maneuver-equivalent-angle
+    that corresponds to the NMAN time before the dwell start.
+
+    Parameters
+    ----------
+    npnt_tstart : float
+        Start time of the NPNT dwell.
+    backstop_file : str
+        Backstop file.
+
+    Returns
+    -------
+    float
+        Value of an equavalent maneuver angle between 0 and 180.
+    """
+    states = get_pcad_states(backstop_file)
+    prev_state = states[np.searchsorted(states["tstop"], npnt_tstart)]
+    out = calc_man_angle_for_duration(prev_state['tstop'] - prev_state['tstart'])
+    return out
 
 
 def ir_zone_ok(backstop_file, out=None):
     """
     Check that the high IR zone is all in NMM.
 
-    :param backstop_file: backstop file
-    :param out: optional output file name (for plain text report)
-    :returns: True if high IR zone time is all in NMM, False otherwise
+    Parameters
+    ----------
+    backstop_file : str
+        The path to the backstop file.
+    out : str, optional
+        The path to the output file for the plain text report.
+
+    Returns
+    -------
+    bool
+        True if the high IR zone time is all in NMM, False otherwise.
     """
 
+    # Here, we could use the get_pcad_states convenience function but then
+    # would lose the handy 'obsid' labels.
     states = get_states(backstop_file, state_keys=["pcad_mode", "obsid"])
     bs_cmds = kadi_commands.get_cmds_from_backstop(backstop_file)
 
